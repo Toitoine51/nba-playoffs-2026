e0a536afed643d5b66d0ea8f72fa59bcd42ba8a4
Toitoine51 <Toitoine51@wanadoo.fr>
2026-05-01 19:26:58 +0300

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,20 +1,20 @@
-export const loadResults = () => {
+function loadResults() {
     const saved = localStorage.getItem("nba_results");
     return saved ? JSON.parse(saved) : {};
-};
+}
 
-export const saveResults = (results) => {
+function saveResults(results) {
     localStorage.setItem("nba_results", JSON.stringify(results));
-};
+}
 
-export const loadFetchedKeys = () => {
+function loadFetchedKeys() {
     const saved = localStorage.getItem("nba_fetched_keys");
     return saved ? new Set(JSON.parse(saved)) : new Set();
-};
+}
 
-export const saveFetchedKeys = (keys) => {
+function saveFetchedKeys(keys) {
     localStorage.setItem(
         "nba_fetched_keys",
         JSON.stringify([...keys])
     );
-};
+}
