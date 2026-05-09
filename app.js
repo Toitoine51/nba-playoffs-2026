4925456120d68435e49c0aa1033947508e0059b1
Toitoine51 <280910880+Toitoine51@users.noreply.github.com>
2026-05-09 14:02:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -590,7 +590,7 @@ function App() {
 
             {tab === "article" && (
                 <div style={{ padding: 10, maxWidth: 700 }}>
-                    {true && (
+                    {false && (
                         <div style={{ marginBottom: 16, display: "flex", gap: 8 }}>
                             <input
                                 type="date"
