75871b6ad601875c3618e1b5cc29786f1105ac3c
Toitoine51 <280910880+Toitoine51@users.noreply.github.com>
2026-05-11 18:50:26 +0200

    Delete api.js

--- api.js
+++
@@ -1,6 +0,0 @@
-async function fetchMatches(startStr) {
-    const res = await fetch(
-        `https://scrap2.toitoine51.workers.dev/?start=${startStr}&days=3`
-    );
-    return await res.json();
-}
